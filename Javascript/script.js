127abdfb96ffbcac2f109b9f9c4220f91982c7f9
Nestor Ingles <nestor1ingles@gmail.com>
2023-07-30 14:08:54 -0400

    add javascript basics and functions

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
